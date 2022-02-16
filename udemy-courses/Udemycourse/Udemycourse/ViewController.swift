a51c42d476f430fb26841083de6d8b10b9db9f10
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2022-02-15 22:20:35 -0500

    ViewController updated to fix the xcode crash issue

--- udemy-courses/Udemycourse/Udemycourse/ViewController.swift
+++ udemy-courses/Udemycourse/Udemycourse/ViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by NAVJOT SINGH on 2022-02-08.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -27,38 +26,23 @@ class ViewController: UIViewController {
         default:
             label.text = "Default"
         }
-                
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "tologin"{
+            let nextVC = segue.destination as! Second_VC
+            nextVC.labelText = "Hello set from segue"
+            
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
         
-//        let box  = UIView(frame: CGRect.zero)
-//        box.backgroundColor = UIColor.red
-//        //self.view.addSubview(box)
-//        box.translatesAutoresizingMaskIntoConstraints = false
-//
-//        box.heightAnchor.constraint(equalToConstant: 100).isActive = true
-//        box.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive =  true
-//        box.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-//
-//        let widthConstraint = NSLayoutConstraint(item: box,
-//                                                 attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1.0, constant: 150)
-//        box.addConstraint(widthConstraint)
-        
-        let lable:UILabel = UILabel()
-        label.text = "From code"
-        label.font = UIFont.systemFont(ofSize: 24)
-        label.textAlignment = NSTextAlignment.center
-        label.backgroundColor = UIColor.brown
-        
-        let frame = CGRect(x: 20, y: 130, width: self.view.frame.size.width, height: 48)
-        label.frame = frame
-        
-        // segmented controls = radio button, etc
-        
-        self.view.addSubview(lable)
+    
     }
     
     // sender is who trigger it
