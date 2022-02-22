3d73fee4cdc3d4be807f9ba291d868da6ecec6eb
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2022-02-21 21:34:48 -0500

    TableView cell finisihed
    Custom TableViewCell implemented! and TableView storyboard added.

---
+++ udemy-courses/AmirJCourse/Section1-2-3/Udemycourse/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  Udemycourse
+//
+//  Created by NAVJOT SINGH on 2022-02-21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    //2 custom cells must have unique withIdentifier id
+    @IBOutlet weak var imgShare: UIImageView!
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
+     
+    
+    
+}
---
+++ udemy-courses/AmirJCourse/Section1-2-3/Udemycourse/TableView_VC.swift
@@ -0,0 +1,66 @@
+//
+//  TableView_VC.swift
+//  Udemycourse
+//
+//  Created by NAVJOT SINGH on 2022-02-20.
+//
+
+import Foundation
+import UIKit
+
+class TableView_vc:ViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    var countries = ["CANADA", "US", "PUNJAB", "Pakistan", "QUEBEC", "NewZeaLand", "Australia"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "reuseTitle")
+        //cell?.textLabel?.text = countries[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseTitle") as! TableViewCell
+        
+        cell.label1.text = "Lable1"
+        cell.label2.text = "Label2"
+        
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected \(countries[indexPath.row])")
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        //if-else
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            countries.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.left)
+
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Are you sure?"
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad();
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
